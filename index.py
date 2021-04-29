19db6f43697f32e5ea79c0cf9a09d049b476a577
darius-danaei <darius.danaei@gmail.com>
2021-04-29 15:44:25 +0100

    Darius First Commit

--- index.py
+++ index.py
@@ -12,27 +12,28 @@ from location_monitoring.data_utils import (
 )
 
 
-def get_days():
-    return fetch_days_list()
+# def get_days():
+#     return fetch_days_list()
 
 
-def get_boxes():
-    return fetch_boxes_list()
+# def get_boxes():
+#     return fetch_boxes_list()
 
 
-def get_misplaced_boxes(day, clear_cache=False):
-    return fetch_misplaced_boxes(day, clear_cache=clear_cache)
+# def get_misplaced_boxes(day, clear_cache=False):
+    # return fetch_misplaced_boxes(day, clear_cache=clear_cache)
 
 
 st.set_page_config(page_title="ItemIT Box Locations", layout="wide")
 
-days = get_days()
-boxes = get_boxes()
+days = fetch_days_list()
+boxes = fetch_boxes_list()
 
 st.write(
     """
     # ItemIT box location monitoring
     ## Misplaced boxes:
+    ### Hello World
 """
 )
 
@@ -46,9 +47,9 @@ st.sidebar.write(f"""__Last updated:__ {last_updated}""")
 
 with st.spinner("Loading data from ItemIT"):
     if clear_cache:
-        df = get_misplaced_boxes(day=day, clear_cache=True)
+        df = fetch_misplaced_boxes(day=day, clear_cache=True)
     else:
-        df = get_misplaced_boxes(day=day)
+        df = fetch_misplaced_boxes(day=day)
 
 st.table(df)
 
